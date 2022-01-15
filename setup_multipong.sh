ee4b39c2b656ba5f0a700ec793eaade546eec54c
praecipue <43908213+praecipue@users.noreply.github.com>
2022-01-15 13:48:44 +0100

    Setup for multipong (example cmd migt be wrong)
    Recorded "int_rew" is 0.0 all the time

---
+++ setup_multipong.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -eu
+
+#PYTHON_EXEC=python3.7
+PYTHON_EXEC=python3.6
+
+PYSUFFIX=$(basename $(realpath $(which ${PYTHON_EXEC})) | tr -d -c '[0-9]')
+#PYSUFFIX=$(echo ${PYTHON_EXEC} | tr -d -c '[0-9]')
+
+# unrar-nonfree required only for unpacking roms.rar below
+apt-get install -y ${PYTHON_EXEC}-venv unrar # might require sudo
+
+${PYTHON_EXEC} -m venv ./venv
+
+. ./venv/bin/activate
+
+PIPINSTALL="${PYTHON_EXEC} -m pip install --progress-bar off"
+#PIPINSTALL="${PYTHON_EXEC} -m pip install"
+
+${PYTHON_EXEC} -m pip -q install --upgrade pip
+
+${PIPINSTALL} -r requirements/multi_pong${PYSUFFIX}_stage1.txt
+${PIPINSTALL} -r requirements/multi_pong${PYSUFFIX}_stage2.txt
+
+wget -q -O roms.rar http://www.atarimania.com/roms/Roms.rar
+unrar e roms.rar
+unzip -q ROMS.zip
+
+${PYTHON_EXEC} -m retro.import ROMS/
+
+echo "Done. You can run multipong experiment with"
+echo ". ./venv/bin/activate && ${PYTHON_EXEC} run.py --env 'PongNoFrameskip-v0' --env_kind 'retro_multi' --exp_name 'pongidf' --feat_learning 'idf'"
